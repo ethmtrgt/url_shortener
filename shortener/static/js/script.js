e3ad4a094df408402f202031cb2c10068a85ac6d
Ethem <ethmtrgt@gmail.com>
2021-10-11 20:30:44 +0300

    feat: display error messages

--- shortener/static/js/script.js
+++ shortener/static/js/script.js
@@ -1,18 +1,23 @@
 function formSubmit(formdata) {
     event.preventDefault();
     $.ajax({
-      type:"POST",
-      url:"/shorten",
-      data:  {
-        alias: formdata['alias'].value,
-        long_url: formdata['long_url'].value,
-        csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val()
-    },
-      success: function(data){
-          $('#output').css('display','block')
-          let result=$('#output a');
-          result.attr('href',data['result'])
-          result.text(data['result'])
-    }
+        type: "POST",
+        url: "/shorten",
+        data: {
+            alias: formdata['alias'].value,
+            long_url: formdata['long_url'].value,
+            csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val()
+        },
+        success: function(data){
+            if (data.status == 'success'){
+                $('#output').css('display','block')
+                let result = $('#output a');
+                let shortUrl = `${window.location.origin}/${data.result}`
+                result.attr('href', shortUrl)
+                result.text(shortUrl)
+            } else {
+                alert(data.message)
+            }
+        }
     });
-  }
+}
